ceab954bf0da96b0039e3f2ba77fb785d22f2915
Frode Oijord <foijord@gmail.com>
2019-09-06 10:48:16 +0000

    misc

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,5 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda
+
+export CUDACXX=/usr/local/cuda-10.1/bin/nvcc
