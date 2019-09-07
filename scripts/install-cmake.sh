b820c8e0840fdcca3b69022a71b7283d231ccaaa
Frode Oijord <foijord@gmail.com>
2019-09-07 22:19:14 +0000

    refactor install scripts

---
+++ scripts/install-cmake.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# CMake
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
+sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ bionic main"
+sudo apt-get update
+sudo apt-get install -y cmake
---
+++ scripts/install-gli-glm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/g-truc/gli.git
+git clone https://github.com/g-truc/glm.git
